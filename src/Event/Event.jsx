1b109d9cdd9874038df792108a80179922983c50
Ravikumar Daivajna <ravikumar@preceptlabs.com>
2022-10-02 14:24:56 +0530

    make event lg responsive

--- src/Event/Event.jsx
+++ src/Event/Event.jsx
@@ -5,32 +5,37 @@ import { HiOutlineLocationMarker } from "react-icons/hi";
 
 const Event = () => {
   return (
-    <div>
-      <div>
-        <img
-          src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/17d6299f-f287-469c-a403-b8ab9d75aa62/Birthday_cake.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20221002%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20221002T064103Z&X-Amz-Expires=86400&X-Amz-Signature=656ecdb2bc84991904dc45dc84e46138bcd4326088d72da5d27289e3357922ff&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22Birthday%2520cake.png%22&x-id=GetObject"
-          alt="birthday"
-        />
-      </div>
-      <div className="mt-4 ml-6">
-        <div className="text-3xl font-semibold text-primary-purple-dark">
-          Birthday Bash
+    <div className="md:flex h-[90vh] justify-center items-center">
+      <div className="md:flex flex-col lg:flex-row-reverse justify-around">
+        <div>
+          <img
+            className="sm:w-[375px] sm:h-[375px] md:w-[593px] md:h-[593px] lg:w-[500px] lg:h-[500px]"
+            src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/17d6299f-f287-469c-a403-b8ab9d75aa62/Birthday_cake.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20221002%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20221002T064103Z&X-Amz-Expires=86400&X-Amz-Signature=656ecdb2bc84991904dc45dc84e46138bcd4326088d72da5d27289e3357922ff&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22Birthday%2520cake.png%22&x-id=GetObject"
+            alt="birthday"
+          />
         </div>
-        <div className="text-grey-100 mt-2">
-          Hosted by <span className="font-semibold">Elysia</span>
+        <div className="mt-6 ml-8 md:ml-0 self-start lg:h-[500px]">
+          <div>
+            <div className="text-3xl font-semibold text-primary-purple-dark">
+              Birthday Bash
+            </div>
+            <div className="text-grey-100 mt-2">
+              Hosted by <span className="font-semibold">Elysia</span>
+            </div>
+          </div>
+          <div>
+            <EventInfoItem
+              icon={<IoCalendarOutline size={28} color="#8456EC" />}
+              primaryText="18 August 6:00PM"
+              secondaryText="to 19 August 1:00PM UTC +10"
+            />
+            <EventInfoItem
+              icon={<HiOutlineLocationMarker size={28} color="#8456EC" />}
+              primaryText="Street Name"
+              secondaryText="Suburb, State, Postcode"
+            />
+          </div>
         </div>
-      </div>
-      <div>
-        <EventInfoItem
-          icon={<IoCalendarOutline size={28} color="#8456EC" />}
-          primaryText="18 August 6:00PM"
-          secondaryText="to 19 August 1:00PM UTC +10"
-        />
-        <EventInfoItem
-          icon={<HiOutlineLocationMarker size={28} color="#8456EC" />}
-          primaryText="Street Name"
-          secondaryText="Suburb, State, Postcode"
-        />
       </div>
     </div>
   );
--- src/EventInfoItem/EventInfoItem.jsx
+++ src/EventInfoItem/EventInfoItem.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { BsChevronRight } from "react-icons/bs";
 const EventInfoItem = ({ icon, primaryText, secondaryText }) => {
   return (
-    <div className="flex ml-6 m-4">
+    <div className="flex mt-4 md:w-[600px] relative lg:w-[600px] relative">
       <div className="shadow-lg w-16 h-16 flex justify-center items-center rounded-lg ">
         {icon}
       </div>
-      <div className="mt-3 ml-6">
+      <div className="mt-3 ml-4">
         <div className="text-primary-purple-dark font-semibold">
           {primaryText}
         </div>
